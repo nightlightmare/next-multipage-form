b7709ee58f08416edcc26eee0c0d2056ae9edea8
Petr Grishin <pgrishin@quintegro.com>
2023-06-26 23:46:14 +0400

    fix path

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
-const debug = process.env.NODE_ENV !== 'production';
-
 /**
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
   output: 'export',
-  // assetPrefix: './'
-  assetPrefix: !debug ? 'next-multipage-form' : ''
+  assetPrefix: './'
 };
 
 export default nextConfig;
