7e7cf87757c39a8706aa6065fc30617e49725dcd
Petr Grishin <pgrishin@quintegro.com>
2023-06-26 20:50:05 +0400

    next config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
-  }
-}
+  },
+  assetPrefix: './',
+};
 
-module.exports = nextConfig
+export default nextConfig;
